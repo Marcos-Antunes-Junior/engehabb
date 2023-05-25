716e9e21a238c884be270cc040a0fd169216a71f
Marcos-Antunes-Junior <mantunes140397@gmail.com>
2023-05-25 09:33:47 -0300

    "fixed formulario.js"

--- scripts/formulario.js
+++ scripts/formulario.js
@@ -1,6 +1,9 @@
 function myFunction(){
         const valorNome = document.getElementById("nomecompleto").value;
-        if(valorNome == ""){
+        const valorTel = document.getElementById("telefone").value;
+        const valorEmail = document.getElementById("endEmail").value;
+
+        if(valorNome == "" || valorTel == "" || valorEmail == ""){
         alert("Por favor, preencha o formulário antes de enviar.");
         }else{
         alert(`Obrigado, ${valorNome}! Entraremos em contato o mais breve possível.`);
