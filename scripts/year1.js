92621f1d34481e6a25c6deaca0bb112ccc8cd845
Marcos-Antunes-Junior <mantunes140397@gmail.com>
2023-07-17 19:42:49 -0300

    "."

--- scripts/year1.js
+++ scripts/year1.js
@@ -5,6 +5,14 @@ document.querySelector("#contact-button").onclick = function () {
     window.location.href = "contato.html";
 };
 
+document.querySelector(".logo").onclick = function () {
+    window.location.href = "index.html";
+};
+
+document.querySelector("#logo2").onclick = function () {
+    window.location.href = "index.html";
+};
+
 
 
 
